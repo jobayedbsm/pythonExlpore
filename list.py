2801fff01fefb198ee14161c41545d5fb86b9876
jobayed24 <jkhouse117@gmail.com>
2025-03-11 23:50:05 +0600

    search item in list: by index method searching item on list

--- list.py
+++ list.py
@@ -1,4 +1,4 @@
 my_list = ["apple", "banana", "cherry","apple"]
-# want not modify or change existing list
-new_list=my_list[:2]+my_list[3:]
-print(new_list)
+# find the item in list
+finditem=my_list.index('banana')
+print(finditem)
