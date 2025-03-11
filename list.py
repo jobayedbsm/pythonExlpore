a999f55339125ad2acf3a76a3dac7184423fee1f
jobayed24 <jkhouse117@gmail.com>
2025-03-11 23:35:01 +0600

    extend existing list by extend function

--- list.py
+++ list.py
@@ -1,24 +1,6 @@
 my_list = ["apple", "banana", "cherry","apple"]
 
-# insert data in specific index 
-# there are another same function which is insert function
-my_list[1]='jobayed'
-# print(my_list)
+# extend list 
+my_list.extend([100, 200, 300])
 
-# print data through loop
-
-for data in my_list:
-    print(data)
-
-print('is there specific data exist')
-
-if "data" in my_list:
-    print('yes,data is exists there')
-else:
-    print('no, data is not exist in list')
-
-print("add data in list in the last position")
-
-my_list.append('pineapple')
 print(my_list)
-
