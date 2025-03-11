9e93ad6f40eea9914e9d245f5a2fc47b83098788
jobayed24 <jkhouse117@gmail.com>
2025-03-11 23:38:00 +0600

    return new list by concatening and list unpacking method

--- list.py
+++ list.py
@@ -1,6 +1,11 @@
 my_list = ["apple", "banana", "cherry","apple"]
 
-# extend list 
-my_list.extend([100, 200, 300])
 
-print(my_list)
+
+# will return new list
+newList=my_list+[100, 200, 300]
+print(newList)
+
+# will return new list by list unpacking
+newList2=[*my_list,*[100, 200, 300]]
+print(newList2)
