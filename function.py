b62e7a178090a94bd02e8551fc7a6ccd058a9234
jobayed24 <jkhouse117@gmail.com>
2025-03-24 23:46:13 +0600

    pass: used in class and used it in function

--- function.py
+++ function.py
@@ -1,6 +1,23 @@
-def my_function():
-    pass
+class UserAuth:
+    def __init__(self):
+        self.users = {"jobayed": "securepassword"}  # Sample user database
 
-my_function()
+    def login(self, username, password):
+        """Validate user login credentials."""
+        if username in self.users and self.users[username] == password:
+            print("Login successful!")
+        else:
+            print("Invalid username or password.")
 
+    def register(self, username, password):
+        """Register a new user - To be implemented."""
+        pass  # Future implementation for adding users
 
+    def reset_password(self, username):
+        """Reset user password - To be implemented."""
+        pass  # Future password reset logic
+
+# Usage
+auth = UserAuth()
+auth.login("jobayed", "securepassword")  # ✅ Login successful!
+auth.register("newuser", "newpass")      # Currently does nothing
