80b8c7ab6a5d80da3a99096f60fe034a30b58cb4
jobayed24 <jkhouse117@gmail.com>
2025-03-29 14:08:22 +0600

    filter method use for remove all occurrence from list

--- list.py
+++ list.py
@@ -1,7 +1,4 @@
-fruits = ['apple', 'banana', 'cherry',"apple"]
-# list comprehension (Remove all occurrence)
-try:
-    newList=[fruit for fruit in fruits if fruit!="apple"]
-    print(newList)
-except ValueError:
-    print('No value occurrence in the list')
+my_list = [1, 2,3, 4, 5]
+new_list=list(filter(lambda x:x !=2,my_list))
+print(new_list)
+
