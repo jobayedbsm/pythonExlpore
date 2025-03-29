eb8cc2620dd666865f047b90487064ca67b7a43b
jobayed24 <jkhouse117@gmail.com>
2025-03-29 13:47:02 +0600

    remove all occurrence from list by list comprehension

--- list.py
+++ list.py
@@ -1,11 +1,7 @@
-import time
-ticket_queue = [101, 102, 103, 104, 105]
-
-while ticket_queue:
-    current_ticket=ticket_queue.pop(0)
-    print(f"Current ticket",current_ticket)
-    time.sleep(4)
-    if len(ticket_queue)<3:
-        new_ticket=ticket_queue[-1]+1 if ticket_queue else 106
-        ticket_queue.append(new_ticket)
-        print(f"new customer",new_ticket)
+fruits = ['apple', 'banana', 'cherry',"apple"]
+# list comprehension (Remove all occurrence)
+try:
+    newList=[fruit for fruit in fruits if fruit!="apple"]
+    print(newList)
+except ValueError:
+    print('No value occurrence in the list')
