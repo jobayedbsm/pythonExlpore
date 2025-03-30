8fc567248d07a4cab0e338a2410501006a6abe92
jobayed24 <jkhouse117@gmail.com>
2025-03-30 12:33:29 +0600

    list in sort
    key passed function as which is sort time function lambda

--- list.py
+++ list.py
@@ -1,8 +1,5 @@
-# Reversing a Sentence (Words Order)
-sentence="I love you"
-#  first splic the each world
-sentence=sentence.split(" ")
-sentence.reverse()
+students = [("Bob", 20, 3.8),("Alice", 25, 3.5),  ("Charlie", 30, 3.2)]
 
-sentence=" ".join(sentence)
-print(sentence)
+students.sort(key=lambda x: x[1])
+
+print(students)
