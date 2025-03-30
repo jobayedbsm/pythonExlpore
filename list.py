18180a10bd7292dde8950d591e09278c635a0f11
jobayed24 <jkhouse117@gmail.com>
2025-03-30 12:47:37 +0600

    sort list by multiple criteria use minus for decending

--- list.py
+++ list.py
@@ -1,9 +1,6 @@
-employees = [
-    {"name": "Alice", "age": 25, "salary": 5000},
-    {"name": "Bob", "age": 20, "salary": 7000},
-    {"name": "Charlie", "age": 30, "salary": 4500},
-]
+students = [("Alice", 25, 3.5), ("Bob", 20, 3.8), ("Charlie", 30, 3.2), ("David", 20, 3.9)]
 
-employees.sort(key=lambda x: x['salary'])
-
-print(employees)
+# sort by multiple criteria
+# Sort by Age, then by GPA (Descending)
+students.sort(key=lambda x :(x[1],-x[2]))
+print(students)
